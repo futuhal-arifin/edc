348a4de8fc3ec6ed0ff4a58e9db7db05106b26e9
adit <aa.refandi92@gmail.com>
2014-03-23 13:26:13 +0700

    uncomment di androidnya

--- edoocatia-android/src/id/ac/ui/edoocatia/MainActivity.java
+++ edoocatia-android/src/id/ac/ui/edoocatia/MainActivity.java
@@ -6,33 +6,33 @@ import android.view.View;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
-public class MainActivity extends AndroidApplication  {
+public class MainActivity extends AndroidApplication {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-	    cfg.useAccelerometer = false;
-	    cfg.useCompass = false;
-	        
-	    initialize(new Edoocatia(), cfg);
-	      
-//		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.main);
-//		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-//		cfg.useAccelerometer = false;
-//	    cfg.useCompass = false;
-//		cfg.useGL20 = false;
-//
-//		initialize(new Edoocatia(), cfg);
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		cfg.useAccelerometer = false;
+		cfg.useCompass = false;
 
-//		Button play =(Button) findViewById(R.id.buttonPlay);
-//		play.setOnClickListener(l)
+		initialize(new Edoocatia(), cfg);
+		// super.onCreate(savedInstanceState);
+		// setContentView(R.layout.main);
+		// AndroidApplicationConfiguration cfg = new
+		// AndroidApplicationConfiguration();
+		// cfg.useAccelerometer = false;
+		// cfg.useCompass = false;
+		// cfg.useGL20 = false;
+		//
+		// initialize(new Edoocatia(), cfg);
+
+		// Button play =(Button) findViewById(R.id.buttonPlay);
+		// play.setOnClickListener(l)
 	}
-//
-//	public void ButtonPlay_OnClick(View v) {
-//		Intent i = new Intent(MainActivity.this, Intro.class);
-//		startActivity(i);
-//		finish();
-//	}
+	//
+	// public void ButtonPlay_OnClick(View v) {
+	// Intent i = new Intent(MainActivity.this, Intro.class);
+	// startActivity(i);
+	// finish();
+	// }
 
 }
