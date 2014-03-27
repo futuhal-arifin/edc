8b7ded6d530aca066d2a3bcc23a9bd4972313b52
inassjunus <inas.sofiyah@ui.ac.id>
2014-03-27 12:45:19 +0700

    Update MainMenuController.java

--- edoocatia/src/id/ac/ui/edoocatia/controller/MainMenuController.java
+++ edoocatia/src/id/ac/ui/edoocatia/controller/MainMenuController.java
@@ -1,11 +1,17 @@
 package id.ac.ui.edoocatia.controller;
 
+import id.ac.ui.edoocatia.Edoocatia;
 import id.ac.ui.edoocatia.screen.MainMenuScreen;
+import id.ac.ui.edoocatia.screen.SplashScreen;
 
 public class MainMenuController {
+	
+	MainMenuScreen screen;
+	Edoocatia app;
 
 	public MainMenuController(MainMenuScreen screen) {
-		// TODO Auto-generated constructor stub
+		this.screen = screen;
+		app = screen.getApp();
 	}
 
 	public void processInput() {
