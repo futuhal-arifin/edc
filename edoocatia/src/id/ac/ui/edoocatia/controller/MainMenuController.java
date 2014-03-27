675b5aa6a8749dd84f8f1cd72dfed8dfe9cadf4d
inassjunus <inas.sofiyah@ui.ac.id>
2014-03-27 12:44:07 +0700

    Create MainMenuController.java

---
+++ edoocatia/src/id/ac/ui/edoocatia/controller/MainMenuController.java
@@ -0,0 +1,16 @@
+package id.ac.ui.edoocatia.controller;
+
+import id.ac.ui.edoocatia.screen.MainMenuScreen;
+
+public class MainMenuController {
+
+	public MainMenuController(MainMenuScreen screen) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void processInput() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
