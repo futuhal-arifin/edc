5a8ac34c178148c063588cb6f97cb8f5169f87ea
Inas Junus <inas.sofiyah@ui.ac.id>
2014-03-30 11:58:23 +0700

    update intro

--- edoocatia/src/id/ac/ui/edoocatia/screen/IntroScreen.java
+++ edoocatia/src/id/ac/ui/edoocatia/screen/IntroScreen.java
@@ -12,27 +12,20 @@ import id.ac.ui.edoocatia.util.AbstractScreen;
 
 public class IntroScreen extends AbstractScreen {
 	
-	private final int SCENE_NUMBER = 16;
+	private final int SCENE_NUMBER = 20;
 	private Texture[] sceneIntro = new Texture[SCENE_NUMBER];
-	/*
-	private Texture background;
-	private Texture background2;
-	private Texture background3;
-	private Texture background4;
-	private Texture background5;
-	private Texture background6;
-	private Texture background7;
-	*/
+	
 	private long startTime;
-	//private long endTime;
+	
 	private int rendCount;
-	final int SCENE_TIME = 1500;
-	/*
-	final int background2Time = 3000;
-	final int background3Time = 4500;
-	final int background4Time = 6000;
-	final int background5Time = 7500;
-	*/
+	
+	private final int SCENE_TIME_DEFAULT = 2000;
+	private final int SCENE_TEXT_0 = 4000;
+	private final int SCENE_TEXT_4 = 4000;
+	private final int SCENE_TEXT_12 = 5000;
+	private final int SCENE_TEXT_19 = 3000;
+	private int current_scene_time = SCENE_TIME_DEFAULT;
+
 	
 	private Music introMusicBg;
 	
@@ -43,18 +36,9 @@ public class IntroScreen extends AbstractScreen {
 		Texture.setEnforcePotImages(false);
 		
 		for (int idx = 0; idx < this.sceneIntro.length; idx++) {
-			sceneIntro[idx] = new Texture(Gdx.files.internal("data/images/intro/"+(idx+1)+".jpg"));
+			sceneIntro[idx] = new Texture(Gdx.files.internal("data/images/intro/"+(idx)+".jpg"));
 		}
-		
-		/*
-		background = new Texture(Gdx.files.internal("data/images/intro/1.jpg"));
-		background2 = new Texture(Gdx.files.internal("data/images/intro/2.jpg"));
-		background3 = new Texture(Gdx.files.internal("data/images/intro/3.jpg"));
-		background4 = new Texture(Gdx.files.internal("data/images/intro/4.jpg"));
-		background5 = new Texture(Gdx.files.internal("data/images/intro/5.jpg"));
-		background6 = new Texture(Gdx.files.internal("data/images/intro/6.jpg"));
-		background7 = new Texture(Gdx.files.internal("data/images/intro/7.jpg"));
-		*/
+
 		rendCount = 0;
 		startTime = TimeUtils.millis();
 		
@@ -65,15 +49,22 @@ public class IntroScreen extends AbstractScreen {
 			introMusicBg.setLooping(true);
 			introMusicBg.play();
 		} 
-		
 		// !! wajib daftarin controller
 		introController = new IntroController(this);
 	}
 
 	public void render(float delta) {
-		// TODO Auto-generated method stub
+		
+		switch(this.rendCount) {
+			case 0:  current_scene_time = this.SCENE_TEXT_0; break;
+			case 4:  current_scene_time = this.SCENE_TEXT_4; break;
+			case 12:  current_scene_time = this.SCENE_TEXT_12; break;
+			case 19:  current_scene_time = this.SCENE_TEXT_19; break;
+			default: current_scene_time = this.SCENE_TIME_DEFAULT; break;
+		}
+		
 		cam.update();
-
+		
 		// set viewport
 		Gdx.gl.glViewport((int) viewport.x, (int) viewport.y,
 				(int) viewport.width, (int) viewport.height);
@@ -92,54 +83,8 @@ public class IntroScreen extends AbstractScreen {
 		batcher.setProjectionMatrix(cam.combined);
 		batcher.begin();
 
-		/*
-		 * gambar background di sini
-		 */
-		
 		batcher.draw(sceneIntro[rendCount], 0, 0);
 		
-		/*
-		switch (rendCount) {
-		case 1:
-			batcher.draw(background, 0, 0);
-
-			break;
-
-		case 2:
-			batcher.draw(background2, 0, 0);
-
-			break;
-
-		case 3:
-			batcher.draw(background3, 0, 0);
-
-			break;
-
-		case 4:
-			batcher.draw(background4, 0, 0);
-
-			break;
-
-		case 5:
-			batcher.draw(background5, 0, 0);
-
-			break;
-
-		case 6:
-			batcher.draw(background6, 0, 0);
-
-			break;
-
-		case 7:
-			batcher.draw(background7, 0, 0);
-
-			break;
-
-			
-		default:
-			break;
-		}
-		*/
 		batcher.end();
 
 		introController.processInput();
@@ -158,7 +103,7 @@ public class IntroScreen extends AbstractScreen {
 	}
 
 	public long getBgTime() {
-		return this.SCENE_TIME;
+		return this.current_scene_time;
 	}
 	
 	public boolean isIntroEnded() {
