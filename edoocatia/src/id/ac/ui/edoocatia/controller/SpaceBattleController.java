255a2b2a670e6e86a691d06638ab997520b82fa1
Inas Junus <inas.sofiyah@ui.ac.id>
2014-06-02 15:48:22 +0700

    ketinggalan

--- edoocatia/src/id/ac/ui/edoocatia/controller/SpaceBattleController.java
+++ edoocatia/src/id/ac/ui/edoocatia/controller/SpaceBattleController.java
@@ -34,53 +34,56 @@ public class SpaceBattleController {
 			if (data.getCurrentDistance() >= data.getMaxDistance()) {
 				screen.setBattleEnded(true);
 			}
-			if(Gdx.input.justTouched()){
-				Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-				cam.unproject(pos, viewport.x, viewport.y, viewport.width, viewport.height);
+			if(data.isAlienComing()) {
 				
-				if(OverlapTester.pointInRectangle( buttonBounds[screen.LEFT], pos.x, pos.y)){
-					screen.setButtonStatus(true, screen.LEFT);
+			} else {
+				if(Gdx.input.justTouched()){
+					Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+					cam.unproject(pos, viewport.x, viewport.y, viewport.width, viewport.height);
+					
+					if(OverlapTester.pointInRectangle( buttonBounds[screen.LEFT], pos.x, pos.y)){
+						screen.setButtonStatus(true, screen.LEFT);
+					}
+					else if(OverlapTester.pointInRectangle( buttonBounds[screen.RIGHT], pos.x, pos.y)){
+						screen.setButtonStatus(true, screen.RIGHT);
+					}
 				}
-				else if(OverlapTester.pointInRectangle( buttonBounds[screen.RIGHT], pos.x, pos.y)){
-					screen.setButtonStatus(true, screen.RIGHT);
+				
+				if(Gdx.input.isTouched()){
+					Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+					cam.unproject(pos, viewport.x, viewport.y, viewport.width, viewport.height);
+					if(screen.buttonIsActive(screen.LEFT)){
+						//screen.setLeftButtonPressed(false);
+						if(OverlapTester.pointInRectangle( buttonBounds[screen.LEFT], pos.x, pos.y)){
+								
+							float currentPosition = data.getPesawatXPosition();
+							if(currentPosition >= data.getLeftLimit()-5.0f) {
+								data.setPesawatXPosition(currentPosition-5.0f);
+							} else {
+								data.setPesawatXPosition(data.getLeftLimit());
+							}
+						}
+					}
+					else if(screen.buttonIsActive(screen.RIGHT)){
+						
+						if(OverlapTester.pointInRectangle( buttonBounds[screen.RIGHT], pos.x, pos.y)){
+							float currentPosition = data.getPesawatXPosition();
+							if(currentPosition <= (data.getRightLimit()+5.0f)) {
+								data.setPesawatXPosition(currentPosition+5.0f);
+							} else {
+								data.setPesawatXPosition(data.getRightLimit());
+							}
+						}
+					}
+				} else {
+					if(screen.buttonIsActive(screen.LEFT)){ 
+						screen.setButtonStatus(false, screen.LEFT);
+					} else if(screen.buttonIsActive(screen.RIGHT)){
+						screen.setButtonStatus(false, screen.RIGHT);
+					}
 				}
-				
-				
 			}
 			
-			if(Gdx.input.isTouched()){
-				Vector3 pos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
-				cam.unproject(pos, viewport.x, viewport.y, viewport.width, viewport.height);
-				if(screen.buttonIsActive(screen.LEFT)){
-					//screen.setLeftButtonPressed(false);
-					if(OverlapTester.pointInRectangle( buttonBounds[screen.LEFT], pos.x, pos.y)){
-							
-						float currentPosition = data.getPesawatXPosition();
-						if(currentPosition >= data.getLeftLimit()-5.0f) {
-							data.setPesawatXPosition(currentPosition-5.0f);
-						} else {
-							data.setPesawatXPosition(data.getLeftLimit());
-						}
-					}
-				}
-				else if(screen.buttonIsActive(screen.RIGHT)){
-					
-					if(OverlapTester.pointInRectangle( buttonBounds[screen.RIGHT], pos.x, pos.y)){
-						float currentPosition = data.getPesawatXPosition();
-						if(currentPosition <= (data.getRightLimit()+5.0f)) {
-							data.setPesawatXPosition(currentPosition+5.0f);
-						} else {
-							data.setPesawatXPosition(data.getRightLimit());
-						}
-					}
-				}
-			} else {
-				if(screen.buttonIsActive(screen.LEFT)){ 
-					screen.setButtonStatus(false, screen.LEFT);
-				} else if(screen.buttonIsActive(screen.RIGHT)){
-					screen.setButtonStatus(false, screen.RIGHT);
-				}
-			}
 		}
 	}
 
